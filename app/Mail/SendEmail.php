fd9e87742d1ae4ad4fec943d2f3068d2f8734869
Sahil Gaurav <Sahil.Gaurav@theknowledgeacademy.com>
2023-08-19 09:51:09 +0530

    mail commit

---
+++ app/Mail/SendEmail.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Envelopable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $name;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct()
+    {
+        $this->name = 'sahil';
+    }
+
+    /**
+     * Build the message.
+     */
+    public function build()
+    {
+        return $this->view(view:'email');
+       
+    }
+    }
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Mail\SendEmail;
+use App\Mail\SendMarkdownMail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    // $data = ['name' => "The test Coder"];
+
+    // // Using Laravel Mail Facade
+    // Mail::raw("hii, this is working fine", function ($msg) {
+    //     $msg->to("boyd62364@gmail.com", "Advance Laravel User")
+    //         ->subject("Advance Laravel Series");
+    // });  
+
+    // echo('mail send success');
+
+/* this is use for mailable  whith view file */
+/* Mail::to('test1@test.com', 'New User Test')->send(new SendEmail());
+
+ echo('mailSend sucessfully'); */
+
+
+//markdown email sent
+Mail::to(users:'test1@test.com')
+->send(new SendMarkdownMail());
+
+echo("mailSend sucessfully");
+
+  });
+
